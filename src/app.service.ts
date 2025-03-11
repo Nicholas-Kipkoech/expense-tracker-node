6857e57ca206640e0e21391a1a535412b85a070e
Nicholas Kipkoech <nickey968@gmail.com>
2025-03-11 19:33:39 +0300

    remove unused code block

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,19 +2,6 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { InjectConnection } from '@nestjs/mongoose';
 import { Connection } from 'mongoose';
 
-import * as admin from 'firebase-admin';
-
-import * as fs from 'fs';
-import * as path from 'path';
-
-const serviceAccount = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, '../service-account.json'), 'utf8'),
-) as admin.ServiceAccount; // ✅ Use firebase-admin's type
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
 @Injectable()
 export class AppService implements OnModuleInit {
   constructor(@InjectConnection() private readonly connection: Connection) {}
@@ -26,12 +13,5 @@ export class AppService implements OnModuleInit {
     } catch (error) {
       console.error('❌ Database connection failed', error);
     }
-  }
-
-  async sendNotification(token: string, title: string, body: string) {
-    await admin.messaging().send({
-      token,
-      notification: { title, body },
-    });
   }
 }
